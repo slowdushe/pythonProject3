ca36d9e23bc87289112cb86f4ace20cd0d5b5486
slowdushe <slowdushe@gmail.com>
2024-02-01 01:06:01 +0500

    imtihon

--- Ravshan/models.py
+++ Ravshan/models.py
@@ -1,3 +1,29 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
--- Ravshan/views.py
+++ Ravshan/views.py
@@ -1,3 +1,90 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+from Ravshan.form import UserRegistrationModelForm, UserLoginForm
+
+
+class HomePageView(View):
+    def get(self, request):
+        return render(request, 'shokirov/home.html')
+
+
+class UserRegisterView(View):
+    def get(self, request):
+        form = UserRegistrationModelForm()
+        return render(request, "shokirov/register.html", {"form": form})
+
+    def post(self, request):
+        form = UserRegistrationModelForm(data=request.POST)
+        if form.is_valid():
+            messages.success(request, "User registered successfully")
+            form.save()
+            return redirect('shokirov:login')
+        else:
+            return render(request, "shokirov/register.html", {"form": form})
+
+
+class UserLoginView(View):
+    def get(self, request):
+        form = UserLoginForm()
+        return render(request, "shokirov/login.html", {"form": form})
+
+    def post(self, request):
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                print(request.COOKIES)
+                messages.success(request, "User logged in successfully")
+                return redirect("shokirov:home-page")
+            else:
+                messages.error(request, "Username or password is wrong")
+                return redirect("shokirov:login")
+        else:
+            return render(request, "shokirov/login.html", {"form": form})
+
+
+class UserLogoutView(View):
+    def get(self, request):
+        return render(request, "shokirov/logout.html")
+
+    def post(self, request):
+        logout(request)
+        messages.info(request, "User logged out successfully")
+        return redirect('shokirov:home-page')
+
+
+class UserAboutView(View):
+    def get(self, request):
+        return render(request, "shokirov/about.html")
+
+
+class UserHomeView(View):
+    def get(self, request):
+        return render(request, "shokirov/home.html")
+
+
+class UserPostDetailView(View):
+    def get(self, request):
+        return render(request, "shokirov/post_detail.html")
+
+
+class UserPostConfirmDeleteView(View):
+    def get(self, request):
+        return render(request, "shokirov/post_confirm_delete.html")
+
+
+class UserPostsView(View):
+    def get(self, request):
+        return render(request, "shokirov/user_posts.html")
+
+
+class UserPostsFormView(View):
+    def get(self, request):
+        return render(request, "shokirov/post_form.html")
+
